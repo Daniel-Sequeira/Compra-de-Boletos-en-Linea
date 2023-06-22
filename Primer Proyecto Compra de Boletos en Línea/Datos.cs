1238f80b4efb3f40f263f38825fd5ec8d541f2f1
Daniels <danielsequeira22@gmail.com>
2023-06-22 10:54:02 -0600

    Agregar archivos de proyecto.

---
+++ Primer Proyecto Compra de Boletos en Línea/Datos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primer_Proyecto_Compra_de_Boletos_en_Línea
+{
+     class Datos
+    {
+       public  string[] horarios = {"05:00am", "07:00am", "12:00pm", "03:00pm"};
+        
+         
+
+
+
+
+
+    }
+}
---
+++ Primer Proyecto Compra de Boletos en Línea/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Primer_Proyecto_Compra_de_Boletos_en_Línea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Horarios de Ferry Puntarenas--Paquera");
+            Datos horarios = new Datos();
+            Console.WriteLine(Datos);
+
+
+        }
+    }
+}
