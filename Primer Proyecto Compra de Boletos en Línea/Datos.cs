7ed94a685170d443eec6d00f4af16d0b4308c1bb
Daniels <danielsequeira22@gmail.com>
2023-06-26 17:00:07 -0600

    arreglo clientes

--- Primer Proyecto Compra de Boletos en Línea/Datos.cs
+++ Primer Proyecto Compra de Boletos en Línea/Datos.cs
@@ -11,6 +11,19 @@ namespace Primer_Proyecto_Compra_de_Boletos_en_Línea
         private string[] horario3 = new string[0];
         private string[] horario4 = new string[0];
 
+        private string[] cliente1 = new string[4];
+        private string[] cliente2 = new string[4];
+        private string[] cliente3 = new string[4];
+        private string[] cliente4 = new string[4];
+        private string[] cliente5 = new string[4];
+        private string[] cliente6 = new string[4];
+        private string[] cliente7 = new string[4];
+        private string[] cliente8 = new string[4];
+        private string[] cliente9 = new string[4];
+        private string[] cliente10 = new string[4];
+        private string[] cliente11 = new string[4];
+        private string[] cliente12 = new string[4];
+
         public void HorariosFerry()
         {
             horario1 = new string[] { "09:00 am" };
@@ -23,6 +36,21 @@ namespace Primer_Proyecto_Compra_de_Boletos_en_Línea
             Console.WriteLine("3---" + horario3[0]);
             Console.WriteLine("4---" + horario4[0]);
             Console.WriteLine("5---" + "Salir");
+        }
+        public void ClientesFerry()
+        {
+            cliente1 = new string[] { " Roberto " + " Fernandez Esquivel " + " 101110222 " + " rfernandez@hotmail.com " };
+            cliente2 = new string[] {" Luisa "+" Herrera Perez "+" 202220222 "+" lherrera@gmail.com " };
+            cliente3 = new string[] { " Gabriel " + " Saenz Torres " + " 303330333 " + " gsaenz@yahoo.es " };
+            cliente4 = new string[] { " Daniel " + " Alvarez Solano " + " 404440444 " + " dalvarez@yahoo.com" };
+            cliente5 = new string[] { " Patricia " + " Sevilla Arias" + " 505550555 " + " psevilla25@gmail.com " };
+            cliente6 = new string[] { " Miguel " + " Mora Prado " + " 606660666 " + " mmora@rocketmail.com " };
+            cliente7 = new string[] { " Jorge " + " Salazar Arrieta " + " 707770777 " + " jsalazar88@outlook.com " };
+            cliente8 = new string[] { " Carolina " + " Arias Segura " + " 102210331 " + " carias@outlook.com " };
+            cliente9 = new string[] { " Silvia " + " Monge Lizano " + " 102220332 " + " smonge@hotmail.com " };
+            cliente10 = new string[] { " Sthepanie " + " Quesada Rojas " + " 102230333 " + " squesada17@gmail.com " };
+            cliente11 = new string[] { };
+            cliente12 = new string[] { };
         }
         
     }
