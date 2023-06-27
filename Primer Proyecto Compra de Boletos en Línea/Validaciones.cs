06a0ce0fa775e57a291e2e52fdf348875995f5b8
Fabian Arredondo <fabianarredondo365@gmail.com>
2023-06-27 12:08:06 -0600

    Implmento un ciclo do while para probar el menu

--- Primer Proyecto Compra de Boletos en Línea/Validaciones.cs
+++ Primer Proyecto Compra de Boletos en Línea/Validaciones.cs
@@ -15,34 +15,40 @@ namespace Primer_Proyecto_Compra_de_Boletos_en_Línea
         {
           
            horarios.HorariosFerry(); //Muestra los horarios almacenados en arreglo horarios
-
-            int opcion;
-
-            Console.WriteLine("Elige tu horario con los números 1,2,3,4 o 5 para salir");//Titulo en pantalla
-            opcion = Convert.ToInt32(Console.ReadLine());
-
-            switch (opcion)
+            bool continua = true;
+            do
             {
-                case (1):
-                    Console.WriteLine("Elegiste el Horario 1"); //Debemos agregar codigo de l que sucede en cada opcion
-                    break;
-                case (2):
-                    Console.WriteLine("Elegiste el Horario 2");
-                    break;
-                case (3):
-                    Console.WriteLine("Elegiste el Horario 3");
-                    break;
-                case (4):
-                    Console.WriteLine("Elegiste el Horario 4");
-                    break;
-                case (5):
-                    Console.WriteLine("Fue un placer servirle, adiós");
-                    break;
-                default: Console.WriteLine("Por favor ingrese unicamente 1,2,3,4 o 5 para salir");
-                    break;
+                int opcion;
 
-            }
-            Console.ReadKey();
+                Console.WriteLine("Elige tu horario con los números 1,2,3,4 o 5 para salir");//Titulo en pantalla
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case (1):
+                        Console.WriteLine("Elegiste el Horario 1"); //Debemos agregar codigo de l que sucede en cada opcion
+                        break;
+                    case (2):
+                        Console.WriteLine("Elegiste el Horario 2");
+                        break;
+                    case (3):
+                        Console.WriteLine("Elegiste el Horario 3");
+                        break;
+                    case (4):
+                        Console.WriteLine("Elegiste el Horario 4");
+                        break;
+                    case (5):
+                        continua = false;
+                        Console.WriteLine("Fue un placer servirle, adiós");
+                        break;
+                    default:
+                        Console.WriteLine("Por favor ingrese unicamente 1,2,3,4 o 5 para salir");
+                        break;
+
+                }
+               
+                Console.WriteLine();
+            } while (continua);
 
 
 
