24617b60c888a853c24f99b83f077443c795953a
Daniels <danielsequeira22@gmail.com>
2023-06-30 19:47:43 -0600

    opciones de menu se movieron de validacion a main

--- Primer Proyecto Compra de Boletos en Línea/Program.cs
+++ Primer Proyecto Compra de Boletos en Línea/Program.cs
@@ -4,10 +4,9 @@ namespace Primer_Proyecto_Compra_de_Boletos_en_Línea
 {
     class Program
     {
+        
         static void Main(string[] args)//Main es lo primero que llama y ejecuta el programa.
         {
-            Validaciones MenuPrincipal = new Validaciones(); //Instancia de la clase validaciones  
-           
             Console.WriteLine("////// Horarios Ferry Puntarenas---Paquera//////");//Titulo en pantalla que verá el usuario
 
             Console.WriteLine("1------09:00 am");
@@ -15,28 +14,78 @@ namespace Primer_Proyecto_Compra_de_Boletos_en_Línea
             Console.WriteLine("3------15:00 pm");
             Console.WriteLine("4------20:00 pm");
             Console.WriteLine("5------Salir");
-            MenuPrincipal.getMenu(); //llamada al metodo que contiene menu principal y opciones.
-           
-
-           
-
-
-
-
-
-
             
+            bool continua = true;
+            int opcion;
 
+            do//ejecuta el siguiente código la primera vez
+            {
+                Console.WriteLine("Elige tu horario con los números 1,2,3,4 o 5 para salir");//Titulo en pantalla que verá el usuario
+                string numero = Console.ReadLine(); //almacena la opción selecionada por usuario que será validada en adelante.
 
-       
+                if (int.TryParse(numero, out opcion)) //valida si es verdadero que el usuario digitó 1,2,3,4 o 5, de lo contrario Else.
+                {
+                    Validaciones registro = new Validaciones();
+                    switch (opcion) //Realiza acciones de acuerdo a selección de usuario
+                    {
+                        case (1)://si usurio selecciona opcion 1 realizará
+                            Console.Clear();//limpia la pantalla de datos 
+                            
+                            registro.getRegistroCliente();//Llama al metodo de registro clientes
+                            break;//detiene el ciclo
 
+                        case (2)://si usuario selecciona opcion 2 realizará
+                            Console.Clear();
+                            Console.WriteLine("***Registro de Clientes***");
+                            break;
 
-            
-          
-           
-            
+                        case (3)://si usuario selecciona opcion 3 realizará
+                            Console.Clear();
+                            Console.WriteLine("***Registro de Clientes***");
+                            break;
 
+                        case (4)://si usuario selecciona opcion 4 realizará
+                            Console.Clear();
+                            Console.WriteLine("***Registro de Clientes***");
+
+                            break;
+
+                        case (5)://si usuario selecciona opcion 5 realizará
+                            continua = false; //cambia el valor a falso entonces while no repite y sale
+
+                            break;
+
+                    }
+                }
+                else//Si el usuario digitó cualquier caracter fuera de opcion 1,2,3,4 o 5
+                {
+                    Console.WriteLine("Opcion invalida, digite 1,2,3,4 o 5 para salir");
+                }
+
+                Console.ReadKey();//Evita cierre 
+            } while (continua);//Repite codigo de Do mientras siga siendo true (que el usuario no selleccione 5).
 
         }
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+    
 }
--- Primer Proyecto Compra de Boletos en Línea/Validaciones.cs
+++ Primer Proyecto Compra de Boletos en Línea/Validaciones.cs
@@ -6,84 +6,24 @@ namespace Primer_Proyecto_Compra_de_Boletos_en_Línea
 {
     class Validaciones
     {
-
-
-        Datos horarios = new Datos(); //Instancia de la clase Datos metodo Horarios
-
-
-        public void getMenu()
-        {
-            bool continua = true;
-            int opcion;
-         
-            do//ejecuta el siguiente código la primera vez
-            {
-                Console.WriteLine("Elige tu horario con los números 1,2,3,4 o 5 para salir");//Titulo en pantalla que verá el usuario
-                string numero = Console.ReadLine(); //almacena la opción selecionada por usuario que será validada en adelante.
-
-                if (int.TryParse(numero, out opcion)) //valida si es verdadero que el usuario digitó 1,2,3,4 o 5, de lo contrario Else.
-                {
-                    switch (opcion) //Realiza acciones de acuerdo a selección de usuario
-                    {
-                        case (1)://si usurio selecciona opcion 1 realizará
-                            Console.Clear();//limpia la pantalla de datos 
-                            Console.WriteLine("***Registro de Clientes***");
-                            getRegistroCliente();//Llama al metodo de registro clientes
-                            break;//detiene el ciclo
-
-                        case (2)://si usuario selecciona opcion 2 realizará
-                            Console.Clear();
-                            Console.WriteLine("***Registro de Clientes***");
-                            break;
-
-                        case (3)://si usuario selecciona opcion 3 realizará
-                            Console.Clear();
-                            Console.WriteLine("***Registro de Clientes***");
-                            break;
-
-                        case (4)://si usuario selecciona opcion 4 realizará
-                            Console.Clear();
-                            Console.WriteLine("***Registro de Clientes***");
-
-                            break;
-
-                        case (5)://si usuario selecciona opcion 5 realizará
-                            continua = false; //cambia el valor a falso entonces while no repite y sale
-
-                            break;
-
-                    }
-                }
-                else//Si el usuario digitó cualquier caracter fuera de opcion 1,2,3,4 o 5
-                {
-                    Console.WriteLine("Opcion invalida, digite 1,2,3,4 o 5 para salir");
-                }
-
-                Console.ReadKey();//Evita cierre 
-            } while (continua);//Repite codigo de Do mientras siga siendo true (que el usuario no selleccione 5).
-
-
-        }
-
+        
+        Datos usuario = new Datos(); //Instancia Clase Datos metodo registros
+        
         public void getRegistroCliente()
         {
-            {
-                Datos usuario = new Datos(); //Instancia Clase Datos metodo registros
-                
-                Console.WriteLine("Digite su nombre");
-                usuario.cliente11[0] = Console.ReadLine();
-                Console.WriteLine("Digite sus apellidos");
-                usuario.cliente11[1] = Console.ReadLine();
-                Console.WriteLine("Digite su cédula");
-                usuario.cliente11[2] = Console.ReadLine();
-                Console.WriteLine("Digite su dirección email");
-                usuario.cliente11[3] = Console.ReadLine();
-                Console.ReadKey();
-                
-            }
-
+           Console.WriteLine("***Registro de Clientes***");
+           Console.WriteLine("Digite su nombre");
+           usuario.cliente11[0] = Console.ReadLine();
+           Console.WriteLine("Digite sus apellidos");
+           usuario.cliente11[1] = Console.ReadLine();
+           Console.WriteLine("Digite su cédula");
+           usuario.cliente11[2] = Console.ReadLine();
+           Console.WriteLine("Digite su dirección email");
+           usuario.cliente11[3] = Console.ReadLine();
+           Console.ReadKey();
+               
         }
-        public void Facturacion()
+        public void FacturacionSiNo()
         {
             char elegir;
             Console.WriteLine("Facturar digite (f) ///o (s) para Salir");
