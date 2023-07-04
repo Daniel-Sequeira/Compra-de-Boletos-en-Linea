926851aa2d6193eae2fb17d7f92edff027b4b8e9
Daniels <danielsequeira22@gmail.com>
2023-07-03 19:58:09 -0600

    Codigo sin validaciones terminado

--- Primer Proyecto Compra de Boletos en Línea/Datos.cs
+++ Primer Proyecto Compra de Boletos en Línea/Datos.cs
@@ -26,7 +26,7 @@ namespace Primer_Proyecto_Compra_de_Boletos_en_Línea
         private string[] cliente9 = new string[4];
         private string[] cliente10 = new string[4];
 
-        private string[] cliente11 = new string[4];
+        public string[] cliente11 = new string[4];
         public string[] facturaCliente = new string[4];
 
         private double tarifa;
@@ -50,9 +50,13 @@ namespace Primer_Proyecto_Compra_de_Boletos_en_Línea
         {
             return total = (tarifa + iva);
         }
+        public string Cliente11()
+        {
+            return cliente11[0] +" "+ cliente11[1] + " " + cliente11[2] + " " + cliente11[3];
+        }
         private void ClientesExiten()
         {
-            cliente1 = new string[] { " Roberto ", " Fernandez Esquivel ", " 101110222 ", " rfernandez @hotmail.com " };
+            cliente1 = new string[] { " Roberto "+ " Fernandez Esquivel "+ " 101110222 "+ " rfernandez @hotmail.com " };
             cliente2 = new string[] { " Luisa " + " Herrera Perez " + " 202220222 " + " lherrera@gmail.com " };
             cliente3 = new string[] { " Gabriel " + " Saenz Torres " + " 303330333 " + " gsaenz@yahoo.es " };
             cliente4 = new string[] { " Daniel " + " Alvarez Solano " + " 404440444 " + " dalvarez@yahoo.com " };
@@ -62,6 +66,7 @@ namespace Primer_Proyecto_Compra_de_Boletos_en_Línea
             cliente8 = new string[] { " Carolina " + " Arias Segura " + " 102210331 " + " carias@outlook.com " };
             cliente9 = new string[] { " Silvia " + " Monge Lizano " + " 102220332 " + " smonge@hotmail.com " };
             cliente10 = new string[] { " Sthepanie " + " Quesada Rojas " + " 102230333 " + " squesada17@gmail.com " };
+           
 
         }
 
@@ -125,9 +130,56 @@ namespace Primer_Proyecto_Compra_de_Boletos_en_Línea
                 if (imprime == "g" || imprime == "G")
                 {
                     Console.Clear();
+                    Console.WriteLine("     Los que viajan Puntarenas---Paquera    ");
                     ClientesExiten();
-                    Console.WriteLine(cliente1[0] + cliente1[1] + cliente1[2] + cliente1[3]);
+
+                    foreach (string item in cliente1)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    foreach (string item in cliente2)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    foreach (string item in cliente3)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    foreach (string item in cliente4)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    foreach (string item in cliente5)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    foreach (string item in cliente6)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    foreach (string item in cliente6)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    foreach (string item in cliente7)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    foreach (string item in cliente8)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    foreach (string item in cliente9)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    foreach (string item in cliente10)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine(Cliente11());
                     Console.ReadKey();
+
 
                 }
                 if (imprime == "s" || imprime == "S")
